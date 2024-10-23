1c44e12fd4ee08760b8e9730c515c3fd6259eadf
jottiana <11.paradise.avenue@gmail.com>
2024-10-23 19:43:14 +0200

    added popular cocktail carousel

---
+++ client/src/components/PopularCocktailCarousel/PopularCocktailCarousel.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import "./PopularCocktailCarousel.css";
+
+const PopularCocktailCarousel = () => {
+  const [popularCocktails, setPopularCocktails] = useState([]);
+
+  useEffect(() => {
+    const fetchPopularCocktails = async () => {
+      try {
+        const response = await fetch(
+          "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail",
+        );
+        const data = await response.json();
+        const topCocktails = data.drinks.slice(0, 10);
+
+        const detailedCocktails = await Promise.all(
+          topCocktails.map(async (drink) => {
+            const res = await fetch(
+              `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drink.idDrink}`,
+            );
+            const details = await res.json();
+            return details.drinks[0];
+          }),
+        );
+
+        setPopularCocktails(detailedCocktails);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des cocktails :", error);
+      }
+    };
+
+    fetchPopularCocktails();
+  }, []);
+
+  return (
+    <section className="carousel">
+      <div className="scroll-carousel">
+        <div className="carousel-track">
+          {popularCocktails.map((cocktail, index) => (
+            <div key={cocktail.idDrink} className="carousel-item">
+              <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
+              <h3>{cocktail.strDrink}</h3>
+              {/* <p>{cocktail.strInstructions}</p> */}
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PopularCocktailCarousel;
