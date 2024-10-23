a6e804dbcf8dbe7a651215caa2271f2e25e3808e
jottiana <11.paradise.avenue@gmail.com>
2024-10-23 19:29:29 +0200

    added card component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import Header from "./components/header";
 import Nav from "./components/nav";
+import CocktailCard from "./components/CocktailCard/CocktailCard";
 
 function App() {
   return (
     <>
       <Header />
       <Nav />
+
+      <div className="App">
+        <CocktailCard cocktailId="11007" />
+      </div>
     </>
   );
 }
---
+++ client/src/components/CocktailCard/CocktailCard.tsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import "./CocktailCard.css";
+
+const CocktailCard = ({ cocktailId }) => {
+  const [cocktail, setCocktail] = useState(null);
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(() => {
+    const fetchCocktailDetails = async () => {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`,
+        );
+        const data = await response.json();
+        setCocktail(data.drinks[0]);
+      } catch (error) {
+        console.error(
+          "Erreur lors de la récupération des détails du cocktail :",
+          error,
+        );
+      }
+    };
+
+    fetchCocktailDetails();
+  }, [cocktailId]);
+
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+  };
+
+  if (!cocktail) {
+    return <div>Chargement...</div>;
+  }
+
+  return (
+    <div className="cocktail-card-container">
+      <div className="cocktail-card">
+        <img
+          src={cocktail.strDrinkThumb}
+          alt={cocktail.strDrink}
+          className="cocktail-image"
+        />
+        <div className="cocktail-info">
+          <h3>{cocktail.strDrink}</h3>
+          <p>{cocktail.strInstructions}</p>
+        </div>
+        <button
+          type="button"
+          className={`favorite-button ${isFavorite ? "favorited" : ""}`}
+          onClick={toggleFavorite}
+        >
+          {isFavorite ? "❤️" : "🤍"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CocktailCard;
