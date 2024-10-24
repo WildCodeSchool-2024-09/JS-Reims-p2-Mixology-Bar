26b935acec3d976fcc144c3c940da24b9ad02542
paulcnl <paul.canarelli.2504@gmail.com>
2024-10-24 09:55:42 +0200

    creation de pages

---
+++ client/src/pages/Compte.tsx
@@ -0,0 +1,14 @@
+import "./Compte.css";
+import Header from "../components/header";
+import Nav from "../components/nav";
+
+function Avatar() {
+  return (
+    <>
+      <Header />
+      <Nav />
+    </>
+  );
+}
+
+export default Avatar;
---
+++ client/src/pages/Favories.tsx
@@ -0,0 +1,14 @@
+import "./Favories.css";
+import Header from "../components/header";
+import Nav from "../components/nav";
+
+function Fav() {
+  return (
+    <>
+      <Header />
+      <Nav />
+    </>
+  );
+}
+
+export default Fav;
