5633756a5259a478790624ba050f82dcbade43fb
jottiana <11.paradise.avenue@gmail.com>
2024-10-25 09:40:55 +0200

    modified popular cocktails carousel

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import PopularCocktailCarousel from "./components/PopularCocktailCarousel/PopularCocktailCarousel";
 import Header from "./components/header";
 import Nav from "./components/nav";
-import PopularCocktailCarousel from "./components/PopularCocktailCarousel/PopularCocktailCarousel";
 
 function App() {
   return (
     <>
       <Header />
       <Nav />
-      <PopularCocktailCarousel/>
+      <PopularCocktailCarousel />
       <Outlet />
     </>
   );
