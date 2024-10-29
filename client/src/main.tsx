92da81430fffc149ce194e927801f456a8d32663
Samvel6 <samvel.saroukhanyan@gmail.com>
2024-10-29 16:39:50 +0100

    cocktaildetails

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,6 +8,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 // Import the main app component
 import App from "./App";
 import CocktailDetails from "./components/cocktailDetails/CocktailDetails";
+import Home from "./pages/Home/Home";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
@@ -24,10 +25,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />, // Renders the App component for the home page
     children: [
-      // {
-      //   index: true,
-      //   element: <Home />,
-      // },
+      {
+        index: true,
+        element: <Home />,
+      },
       {
         path: "/cocktail/:id",
         element: <CocktailDetails />,
