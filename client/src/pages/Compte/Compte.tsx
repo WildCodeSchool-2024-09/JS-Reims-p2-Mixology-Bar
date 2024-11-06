7685bef0df7490d9563f0915f2311f5298ddc5cc
paulcnl <paul.canarelli.2504@gmail.com>
2024-11-06 15:14:04 +0100

    fixe compte

--- client/src/pages/Compte/Compte.tsx
+++ client/src/pages/Compte/Compte.tsx
@@ -3,62 +3,64 @@ import "./Compte.css";
 function Compte() {
   return (
     <>
-      <div className="desktop-compte">
-        <div className="compte">
-          <h1>Connexion</h1>
+      <div className="img">
+        <div className="desktop-compte">
+          <div className="compte">
+            <h1>Connexion</h1>
 
-          <div className="inputs-compte">
-            <p>nom d'utilisateur</p>
-            <input
-              type="text"
-              id="nom d'utilisateur"
-              name="nom d'utilisateur"
-              placeholder="nom"
-              className="imput-compte"
-            />
-            <p>mot de passe</p>
-            <input
-              type="text"
-              id="mot de passe"
-              name="mot de passe"
-              placeholder="......"
-              className="imput-compte"
-            />
-            <button type="button" className="button-compte">
-              connexion
-            </button>
-          </div>
-          <hr />
-          <h1>Inscription</h1>
+            <div className="inputs-compte">
+              <p>nom d'utilisateur</p>
+              <input
+                type="text"
+                id="nom d'utilisateur"
+                name="nom d'utilisateur"
+                placeholder="nom"
+                className="imput-compte"
+              />
+              <p>mot de passe</p>
+              <input
+                type="text"
+                id="mot de passe"
+                name="mot de passe"
+                placeholder="......"
+                className="imput-compte"
+              />
+              <button type="button" className="button-compte">
+                connexion
+              </button>
+            </div>
+            <hr />
+            <h1>Inscription</h1>
 
-          <div className="inputs-compte">
-            <p>nom d'utilisateur</p>
-            <input
-              type="text"
-              id="nom d'utilisateur"
-              name="nom d'utilisateur"
-              placeholder="nom"
-              className="imput-compte"
-            />
-            <p>mot de passe</p>
-            <input
-              type="text"
-              id="mot de passe"
-              name="mot de passe"
-              placeholder="......"
-              className="imput-compte"
-            />
-            <p>confirmation mot de passe</p>
-            <input
-              type="text"
-              id="mot de passe"
-              name="mot de passe"
-              placeholder="......"
-              className="imput-compte"
-            />
-            <button type="button" className="button-compte">
-              inscription
-            </button>
+            <div className="inputs-compte">
+              <p>nom d'utilisateur</p>
+              <input
+                type="text"
+                id="nom d'utilisateur"
+                name="nom d'utilisateur"
+                placeholder="nom"
+                className="imput-compte"
+              />
+              <p>mot de passe</p>
+              <input
+                type="text"
+                id="mot de passe"
+                name="mot de passe"
+                placeholder="......"
+                className="imput-compte"
+              />
+              <p>confirmation mot de passe</p>
+              <input
+                type="text"
+                id="mot de passe"
+                name="mot de passe"
+                placeholder="......"
+                className="imput-compte"
+              />
+              <button type="button" className="button-compte">
+                inscription
+              </button>
+            </div>
           </div>
         </div>
       </div>
