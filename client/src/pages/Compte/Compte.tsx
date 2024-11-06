9c6a78985b542bdbc42b7d7092cc36f5dec079c5
paulcnl <paul.canarelli.2504@gmail.com>
2024-11-06 12:28:06 +0100

    compte

--- client/src/pages/Compte/Compte.tsx
+++ client/src/pages/Compte/Compte.tsx
@@ -3,8 +3,64 @@ import "./Compte.css";
 function Compte() {
   return (
     <>
-      <div className="compte">
-        <h1>Connexion </h1>
+      <div className="desktop-compte">
+        <div className="compte">
+          <h1>Connexion</h1>
+
+          <div className="inputs-compte">
+            <p>nom d'utilisateur</p>
+            <input
+              type="text"
+              id="nom d'utilisateur"
+              name="nom d'utilisateur"
+              placeholder="nom"
+              className="imput-compte"
+            />
+            <p>mot de passe</p>
+            <input
+              type="text"
+              id="mot de passe"
+              name="mot de passe"
+              placeholder="......"
+              className="imput-compte"
+            />
+            <button type="button" className="button-compte">
+              connexion
+            </button>
+          </div>
+          <hr />
+          <h1>Inscription</h1>
+
+          <div className="inputs-compte">
+            <p>nom d'utilisateur</p>
+            <input
+              type="text"
+              id="nom d'utilisateur"
+              name="nom d'utilisateur"
+              placeholder="nom"
+              className="imput-compte"
+            />
+            <p>mot de passe</p>
+            <input
+              type="text"
+              id="mot de passe"
+              name="mot de passe"
+              placeholder="......"
+              className="imput-compte"
+            />
+            <p>confirmation mot de passe</p>
+            <input
+              type="text"
+              id="mot de passe"
+              name="mot de passe"
+              placeholder="......"
+              className="imput-compte"
+            />
+            <button type="button" className="button-compte">
+              inscription
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
