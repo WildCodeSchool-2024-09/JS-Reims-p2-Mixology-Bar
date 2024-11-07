e860ac8416b253d36feae1f2efda4b58dc4f00a0
paulcnl <paul.canarelli.2504@gmail.com>
2024-11-07 12:31:54 +0100

    nav-in-header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,10 +1,27 @@
 import "./Header.css";
+import { Link } from "react-router-dom";
+import home from "../../assets/images/Home.png";
+import search from "../../assets/images/search.png";
+import fav from "../../assets/images/Heart-desk.png";
+import compte from "../../assets/images/avatar.png";
 
 function Header() {
   return (
     <>
       <header>
+        <Link to="/home">
+          <img src={home} alt="" />
+        </Link>
+        <Link to="/search">
+          <img src={search} alt="" />
+        </Link>
         <h1>Mixology Bar</h1>
+        <Link to="/favorite">
+          <img src={fav} alt="" />
+        </Link>
+        <Link to="/compte">
+          <img src={compte} alt="" />
+        </Link>
       </header>
     </>
   );
