7687f78049f3b5964ff0ecd9ae435701185b3036
jottiana <11.paradise.avenue@gmail.com>
2024-11-14 15:42:40 +0100

    rework css and modified Search

--- client/src/pages/Search/Search.tsx
+++ client/src/pages/Search/Search.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./Search.css";
 import CocktailCard from "../../components/CocktailCard/CocktailCard";
 
